aa77adee4cdd10e3290159c873c9264e6bc7c49b
David Amos <somacdivad@gmail.com>
2019-05-20 10:08:15 -0500

    Refactor make_ratio and make_constant

--- tnp/functions/make_expressions.py
+++ tnp/functions/make_expressions.py
@@ -4,40 +4,38 @@ from tnp.classes.conjecture_class import Conjecture
 from tnp.functions.get_graph_data import get_graph_data
 
 
-def make_ratio(hyp, target, invariant, bound):
-    graphs = get_graph_data()
-    graphs = [graphs[G] for G in graphs if hyp(graphs[G]) is True]
-    ratios = []
-    for G in graphs:
-        ratios.append(Fraction(G[target] / G[invariant]).limit_denominator(1000))
-    if bound == "upper" and ratios != []:
-        if max(ratios) == 1:
-            return Conjecture(hyp, target, " <= ", f"{invariant}")
-        else:
-            return Conjecture(hyp, target, " <= ", f"{max(ratios)} * {invariant}")
-    elif bound == "lower" and ratios != []:
-        if min(ratios) == 1:
-            return Conjecture(hyp, target, " >= ", f"{invariant}")
-        else:
-            return Conjecture(hyp, target, " >= ", f"{min(ratios)} * {invariant}")
+def make_ratio(hypotheses, target, invariant, bound):
+    bound = bound.lower()
+    if bound not in ("upper", "lower"):
+        raise ValueError("bound must be one of 'upper' or 'lower'")
+
+    graph_data = get_graph_data()
+    graphs_satisfying_hypotheses = (graph_data[G] for G in graph_data if hypotheses(graph_data[G]) is True)
+    ratios = tuple(Fraction(G[target] / G[invariant]).limit_denominator(1000) for G in graphs_satisfying_hypotheses)
+    if ratios == ():
+        return None
+
+    inequality_symbol = " <= " if bound == "upper" else " >= "
+    ratio = max(ratios) if bound == "upper" else min(ratios)
+    right_hand_side = f"{invariant}" if ratio == 1 else f"{ratio} * {invariant}"
+    return Conjecture(hypotheses, target, inequality_symbol, right_hand_side)
 
 
-def make_constant(hyp, target, invariant, bound):
-    graphs = get_graph_data()
-    graphs = [graphs[G] for G in graphs if hyp(graphs[G]) is True]
-    constants = []
-    for G in graphs:
-        constants.append(G[target] - G[invariant])
-    if bound == "upper" and constants != []:
-        if max(constants) == 0:
-            return Conjecture(hyp, target, " <= ", f"{invariant}")
-        else:
-            return Conjecture(hyp, target, " <= ", f"{invariant} + {max(constants)}")
-    elif bound == "lower" and constants != []:
-        if min(constants) == 0:
-            return Conjecture(hyp, target, " >= ", f"{invariant}")
-        else:
-            return Conjecture(hyp, target, " >= ", f"{invariant} + {min(constants)}")
+def make_constant(hypotheses, target, invariant, bound):
+    bound = bound.lower()
+    if bound not in ("upper", "lower"):
+        raise ValueError("bound must be one of 'upper' or 'lower'")
+
+    graph_data = get_graph_data()
+    graphs_satisfying_hypotheses = (graph_data[G] for G in graph_data if hypotheses(graph_data[G]) is True)
+    constants = tuple(G[target] - G[invariant] for G in graphs_satisfying_hypotheses)
+    if constants == ():
+        return None
+
+    inequality_symbol = " <= " if bound == "upper" else " >= "
+    constant = max(constants) if bound == "upper" else min(constants)
+    right_hand_side = f"{invariant}" if constant == 0 else f"{invariant} + {constant}"
+    return Conjecture(hypotheses, target, inequality_symbol, right_hand_side)
 
 
 def make_constant_two(hyp, target, invariant1, invariant2, bound):
