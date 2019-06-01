7d0cd270349186af546b3923a54ed0828bb521eb
David Amos <somacdivad@gmail.com>
2019-05-31 22:39:05 -0500

    Move more invariants to GrinPy

--- tnp/invariants.py
+++ tnp/invariants.py
@@ -1,14 +1,6 @@
 import grinpy as gp
 
 from tnp._base_classes import _GraphCallable, _NamespaceIterator
-from tnp.graph_data.functions.triameter import triameter
-from tnp.graph_data.functions.topological_indicies import (
-    atom_bond_connectivity_index,
-    augmented_randic_index,
-    harmonic_index,
-    randic_index,
-    sum_connectivity_index,
-)
 
 
 class Invariant(_GraphCallable):
@@ -35,12 +27,12 @@ class _invariants(_NamespaceIterator):
         chromatic_number = Invariant(function=gp.chromatic_number)
         matching_number = Invariant(function=gp.matching_number)
         min_maximal_matching_number = Invariant(function=gp.min_maximal_matching_number)
-        triameter = Invariant(function=triameter)  # TODO: Add to GrinPy
-        randic_index = Invariant(function=randic_index)  # TODO: Add to GrinPy
-        augmented_randic_index = Invariant(function=augmented_randic_index)  # TODO: Add to GrinPy
-        harmonic_index = Invariant(function=harmonic_index)  # TODO: Add to GrinPy
-        atom_bond_connectivity_index = Invariant(function=atom_bond_connectivity_index)  # TODO: Add to GrinPy
-        sum_connectivity_index = Invariant(function=sum_connectivity_index)  # TODO: Add to GrinPy
+        triameter = Invariant(function=gp.triameter)
+        randic_index = Invariant(function=gp.randic_index)
+        augmented_randic_index = Invariant(function=gp.augmented_randic_index)
+        harmonic_index = Invariant(function=gp.harmonic_index)
+        atom_bond_connectivity_index = Invariant(function=gp.atom_bond_connectivity_index)
+        sum_connectivity_index = Invariant(function=gp.sum_connectivity_index)
         min_degree = Invariant(function=gp.min_degree)
         max_degree = Invariant(function=gp.max_degree)
         number_of_min_degree_nodes = Invariant(function=gp.number_of_min_degree_nodes)
